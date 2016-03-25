d94921ea81bf6ed1d40ce3bab017b47c76b5f0c6
Mateusz Pietraszewski <pietraszewski.mateusz@gmail.com>
2016-03-25 16:54:13 +0000

    Main Menu buttons

---
+++ Deep Forest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+    
+    public Button newGameButton;
+    public Button exitButton;
+
+	void Start () {
+        newGameButton = newGameButton.GetComponent<Button>();
+        exitButton = exitButton.GetComponent<Button>();
+	}
+	
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Game");
+    }
+	
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
